c6d27e703a686496630e44ae8f4be8bcf038af62
Holst <notholst@gmail.com>
2019-08-15 13:27:07 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SERVER_IP=$1
+SHA=$2
+
+echo "Deploying sha($SHA) to $SERVER_IP"
+
+scp -o StrictHostKeyChecking=no ./docker-compose.yml ubuntu@$SERVER_IP:/tmp/
+ssh -o StrictHostKeyChecking=no -t ubuntu@35.233.86.108 "EXPORT DH_SHA=$SHA ; cd /tmp/ ; docker-compose pull && docker-compose up -d"
+
+echo "Deployed!"
