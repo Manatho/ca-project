4893ab429cc36af8f0b79f70475b9c595d47efd9
Holst <notholst@gmail.com>
2019-08-15 14:20:54 +0200

    fixed a rookie sh mistake

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 SERVER_IP=$1
 SHA=$2
 
 echo "Deploying sha($SHA) to $SERVER_IP"
 
 scp -o StrictHostKeyChecking=no ./docker-compose.yml ubuntu@$SERVER_IP:/tmp/
-ssh -o StrictHostKeyChecking=no -t ubuntu@35.233.86.108 "export DH_SHA=$SHA ; cd /tmp/ ; docker-compose pull && docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -t ubuntu@$SERVER_IP "export DH_SHA=$SHA ; cd /tmp/ ; docker-compose --verbose pull && docker-compose --verbose up -d"
 
 echo "Deployed!"
