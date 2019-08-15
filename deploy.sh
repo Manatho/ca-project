3aa85411c4417522ebf6824489d3ef20f6bf57b7
Holst <notholst@gmail.com>
2019-08-15 13:39:14 +0200

    fixed the fix the was supposedto fix the other fix

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ SHA=$2
 echo "Deploying sha($SHA) to $SERVER_IP"
 
 scp -o StrictHostKeyChecking=no ./docker-compose.yml ubuntu@$SERVER_IP:/tmp/
-ssh -o StrictHostKeyChecking=no -t ubuntu@35.233.86.108 "EXPORT DH_SHA=$SHA ; cd /tmp/ ; docker-compose pull && docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -t ubuntu@35.233.86.108 "export DH_SHA=$SHA ; cd /tmp/ ; docker-compose pull && docker-compose up -d"
 
 echo "Deployed!"
